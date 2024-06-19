f89b28cfa87c01f491529e876e645ba7db6c001e
Demonis0 <demonisgames@gmail.com>
2024-06-19 08:56:29 +0200

    Launch configuration fix

--- Launch.bat
+++ Launch.bat
@@ -8,10 +8,12 @@ cd MLAPI\.venv\Scripts
 call activate.bat
 cd ..
 cd ..
+pip install -r requirements.txt
 start cmd /k "python -m flask --app main_mlapi run"
 
 
 cd..
 cd MLFRONT
+npm install
 start cmd /k "npm run serve"
 
